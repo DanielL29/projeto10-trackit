b236d29337221c660c129567b92d0b0988a31cfa
Daniel <danielnatsufire@gmail.com>
2022-05-24 12:27:32 -0300

    bibliotecas

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from './../assets/global/GlobalStyle';
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Trackit</h1>
+    </>
+  );
+}
